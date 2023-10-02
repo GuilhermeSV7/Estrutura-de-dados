17a596de650a80eea853cc9cb7a23de0dc0f92ae
Gabriel Oliveira <129561097+GabrielBOMM@users.noreply.github.com>
2023-10-02 09:31:31 -0300

    Update main.c

--- Estrutura-de-dados/Atividade7/main.c
+++ Estrutura-de-dados/Atividade7/main.c
@@ -1,51 +1,55 @@
+// Aplicação de Filha e Pilha.
+// Sistema de Reservas para um Restaurante
+
 #include <stdio.h>
 #include "pilha.h"
 #include "fila.h"
 
 int main() {
-    // Cria uma fila de espera para as pessoas que chegam ao banco
+    // 1º Passo: Cria uma fila de espera para as pessoas que ligam para fazer uma reserva no restaurante.
     Fila *filaDeEspera = criarFila(10);
     
-    // Cria uma pilha de atendimento para as pessoas que estão sendo atendidas
+    //2º Passo: Cria uma pilha de atendimento para as pessoas que estão sendo atendidas
     Pilha *pilhaDeAtendimento = criarPilha(10);
     
-    // Pessoas entram na fila de espera
+    // 3º Passo: Incluir as pessoas entram na fila de espera
     enfileirar(filaDeEspera, 1);
     enfileirar(filaDeEspera, 2);
     enfileirar(filaDeEspera, 3);
     
     printf("Pessoas na fila de espera:\n");
     
-    // Enquanto houver pessoas na fila de espera, continua o loop
+    // 4º Passo: Enquanto houver pessoas na fila de espera, continua o loop
     while (!filaVazia(filaDeEspera)) {
-        // Obtenha a primeira pessoa da fila
+        
+        // 5º Passo: Obtenha a primeira pessoa da fila
         int pessoa = frenteDaFila(filaDeEspera);
         
-        // Imprime uma mensagem indicando que a pessoa entrou na fila de espera
+        // 6º Passo: Imprime uma mensagem indicando que a pessoa entrou na fila de espera
         printf("Pessoa %d entrou na fila de espera.\n", pessoa);
         
-        // Coloca a pessoa na pilha de atendimento
+        // 7º Passo: Coloca a pessoa na pilha de atendimento
         enfileirar(pilhaDeAtendimento, pessoa);
         
-        // Remove a pessoa da fila de espera
+        // 8º Passo: Remove a pessoa da fila de espera
         desenfileirar(filaDeEspera);
     }
     
     printf("\nAtendimento em andamento:\n");
     
-    // Enquanto houver pessoas sendo atendidas, continua o loop
+    // 9º Passo: Enquanto houver pessoas sendo atendidas, continua o loop
     while (!pilhaVazia(pilhaDeAtendimento)) {
         // Obtenha a pessoa no topo da pilha de atendimento
         int pessoa = topoDaPilha(pilhaDeAtendimento);
         
-        // Imprime uma mensagem indicando que a pessoa está sendo atendida
+        // 10º Passo: Imprime uma mensagem indicando que a pessoa está sendo atendida
         printf("Atendendo pessoa %d.\n", pessoa);
         
-        // Remove a pessoa da pilha de atendimento
+        // 11º Passo: Remove a pessoa da pilha de atendimento
         desempilhar(pilhaDeAtendimento);
     }
     
-    // Libera a memória das estruturas de dados (fila e pilha)
+    // 12º Passo: Libera a memória das estruturas de dados (fila e pilha)
     liberarFila(filaDeEspera);
     liberarPilha(pilhaDeAtendimento);
 
